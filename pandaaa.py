af2d92f41749726859ee4d10b288996cff399a94
puppet-coder <sirancheevi.palraj@gmail.com>
2024-12-04 09:18:00 +0530

    Panda data reading

--- pandaaa.py
+++ pandaaa.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-# Create a Series
+# ---------------------Create a Series-------------------------
 s = pd.Series([10, 'test', 30, 40], index=["a", "b", "c", "d"])
 print(s)
 
 # Accessing elements
 print(s["b"])  # 20
 
-#-----------------------------------------------------------
+#-------------------------------------------------------------
 import pandas as pd
 
 # Create a DataFrame
@@ -20,12 +20,25 @@ data = {
 df = pd.DataFrame(data)
 print(df)
 
-# Accessing a column
-print(df["Name"])  # Series with names
-
 
 #-------------------------------------------------------------
 df = pd.read_csv("data.csv")
 print(df.head().to_string())  # View the first 5 rows
 
 #df.to_csv("output.csv", index=False)
+
+#-------------data reading-------------------------------------
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "City": ["New York", "Los Angeles", "Chicago"]
+}
+
+df = pd.DataFrame(data)
+print(df.loc[1])
+print(df.loc[0:1, ["Name", "City"]])
+print(df.loc[1:2,["Name"]])
+print(df.iloc[2,2])
+print(df.iloc[1:2,1:2])
+print(df[df["Age"] > 25]) #condition based rendering
+print(df.loc[df["Age"] > 25, ["Name", "City"]])
